ef141461f4a01272fb15b4c54a15627ef3733448
Bhavik Patel <bhavikcpatel@gmail.com>
2018-06-10 14:10:35 -0400

    feat: add child stack navigation

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import { createBottomTabNavigator } from 'react-navigation';
+import { Text, View, Button } from 'react-native';
+import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>IPL!</Text>
+      <Text>Home!</Text>
+      <Button
+        title="Go to Settings"
+        onPress={() => this.props.navigation.navigate('Settings')}
+      />
+      <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+    </View>
+
+    );
+  }
+}
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Details!</Text>
       </View>
     );
   }
@@ -17,16 +35,33 @@ class SettingsScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
+      <Text>Settings!</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => this.props.navigation.navigate('Home')}
+      />
+      <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+    </View>
     );
   }
 }
 
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Details: DetailsScreen,
+});
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+  Details: DetailsScreen,
+});
 export default createBottomTabNavigator(
   {
-    Home: HomeScreen,
-    Settings: SettingsScreen,
+    Home: HomeStack,
+    Settings: SettingsStack,
   },
   {
     navigationOptions: ({ navigation }) => ({
@@ -45,8 +80,14 @@ export default createBottomTabNavigator(
       },
     }),
     tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    },
+      activeTintColor: '#e91e63',
+      inactiveTintColor: 'white',
+      labelStyle: {
+        fontSize: 12,
+      },
+      style: {
+        backgroundColor: '#ddd',
+      },
+    }
   }
 );
